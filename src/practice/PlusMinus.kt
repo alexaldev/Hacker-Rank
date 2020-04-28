e929bb441c57165d10da7ad1324e4016d0e499c0
Alex Allafi <alex.allafi@skroutz.gr>
2020-04-28 12:41:18 +0300

    Resolve PlusMinus

--- src/practice/PlusMinus.kt
+++ src/practice/PlusMinus.kt
@@ -1,2 +1,31 @@
 package practice
 
+import java.util.*
+
+fun plusMinus(arr: Array<Int>) {
+
+    val size = arr.size.toFloat()
+    var positives = 0f
+    var zeros = 0f
+    var negatives = 0f
+
+    arr.forEach {
+        when {
+            it > 0 -> positives += 1
+            it == 0 -> zeros += 1
+            else -> negatives += 1
+        }
+    }
+
+    print("%.5f \n%.5f \n%.5f".format(positives / size, negatives / size, zeros / size))
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    val arr = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    plusMinus(arr)
+}
